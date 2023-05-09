a23733d82c19756067aa3b29a00a28be70f4db45
William Sena <me@willsena.dev>
2023-05-09 13:54:55 -0300

    Add vue import

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,7 @@
 /// <reference types="vite/client" />
+/// <reference path="node_modules/nuxtjs-phaser/@types/vue.index.d.ts"/>
 /// <reference path="node_modules/nuxtjs-phaser/@types/index.d.ts"/>
 /// <reference path="node_modules/nuxtjs-phaser/@types/global.d.ts"/>
 /// <reference path="node_modules/nuxtjs-phaser/@types/vue-shim.d.ts"/>
+
+declare module 'nuxtjs-phaser/vue.index';
